c1b64d4e583327f80a197fa2f64fec0dd4c0e3d8
rss1937 <2111169@uj.edu.sa>
2025-04-20 21:37:20 +0300

    Update Main.java
    Document decorator pattern usage in Main.java (Stage 3)

--- refactored_version/Main.java
+++ refactored_version/Main.java
@@ -5,7 +5,8 @@ public class Main {
         TaskManager manager = TaskManager.getInstance();
 
         Task basicTask = new Task("Submit project report");
-        Task decoratedTask = new HighPriorityTask(new DeadlineTask(basicTask, LocalDate.of(2025, 4, 30)));
+       // Added decorator pattern: HighPriority + DeadlineTask
+Task decoratedTask = new HighPriorityTask(new DeadlineTask(basicTask, LocalDate.of(2025, 4, 30)));
 
         manager.addTask(decoratedTask);
         manager.showTasks();
